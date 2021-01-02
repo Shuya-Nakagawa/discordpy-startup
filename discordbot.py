2152cc61239a2695136e799354f322796db2a799
Shuya-Nakagawa <74599822+Shuya-Nakagawa@users.noreply.github.com>
2021-01-02 22:42:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,7 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def サムネがつまらない(ctx):
+    await ctx.send('ほならね？')
 
 bot.run(token)
