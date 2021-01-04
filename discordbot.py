d7ebc5f2dcd9b02cf2be59ecf4935c2c7e167c73
Shuya-Nakagawa <74599822+Shuya-Nakagawa@users.noreply.github.com>
2021-01-04 19:10:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,31 +5,19 @@ import traceback
 
 token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client();
+    # インストールした discord.py を読み込む
+import dicord
 
-    @client.event
+# 接続に必要なオブジェクトを生成
+client = discord.Client();
+# メッセージ受信時に動作する処理
+@client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/アイコンがつまらない':
-        await message.channel.send('ほならね？')
-        
-       @client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/アイコンがつまらない':
-        await message.channel.send('ほならね？')
-       @client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/アイコンがつまらない':
-        await message.channel.send('ほならね？')
-    
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
 bot.run(token)
