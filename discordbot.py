076b17c3ddfdfd77e9eb14f78285fa2a6b6d369b
Shuya-Nakagawa <74599822+Shuya-Nakagawa@users.noreply.github.com>
2021-01-04 19:05:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,19 +4,25 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-client = discord.Client()
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@bot.command()
-async def サムネがつまらない(ctx):
-    await ctx.send('ほならね？')
+client = discord.Client();
 
     @client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/アイコンがつまらない':
+        await message.channel.send('ほならね？')
+       @client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/アイコンがつまらない':
+        await message.channel.send('ほならね？')
+       @client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
